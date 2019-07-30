4ae8cfa53237f2da283ec339d581e7cf631f5297
Brian Wong <bwong92@gmail.com>
2019-07-30 07:06:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word_with_each(string)
   end
   new_arr.join(' ')
 end
+
+def reverse_each_word(string)
+  arr = string.split(" ")
+  new_arr = []
+  array.collect do |word|
+    new_arr << string.reverse
+  end
+  new_arr.join(' ')
+end
